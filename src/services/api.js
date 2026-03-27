a6103bc6106921d5a1b4595f77badef203a2e15c
ahntuan <ahntuan284@gmail.com>
2026-03-27 21:15:32 +0700

    fix port BE

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // 1. Khởi tạo một instance của axios
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://localhost:7165/api",
   headers: {
     "Content-Type": "application/json",
   },
